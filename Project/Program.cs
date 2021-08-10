35ed3a175f0b15674cc8428987016c8c0d64c076
Marlon Santos <xsantos.marlonx@gmail.com>
2021-08-10 09:58:57 -0300

    Conclusão do projeto

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Project.Entities;
+using System.Globalization;
+using Project.Entities.Enums;
 
 namespace Project
 {
@@ -6,7 +9,42 @@ namespace Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i + 1} item data:");
+                Console.Write("Product name: ");
+                string prodName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Product product = new Product(prodName, price);
+
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                OrderItem item = new OrderItem(quantity, price, product);
+                order.AddItem(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
         }
     }
 }
